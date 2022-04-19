0edc8a7338890621b6f65bcdb6166774005ea3da
Rizwan Kabir <rizwankabirsizan@gmail.com>
2022-04-19 17:40:22 +0200

    UPDATE: Welcome & Navbar Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Product/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import ProductList from "./pages/ProductList/ProductList";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/productList" element={<ProductList />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import Navbar from "../../components/Product/Navbar/Navbar";
 import Welcome from "../../components/Welcome/Welcome";
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
       <Welcome />
     </div>
   );
